6fda990fce9ee483902d6313275cd968b2903f32
Adis2020 <adiskd22gm@gmail.com>
2023-07-14 21:19:57 +0600

    Added new function which will analisys a text

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
 import os
+import pytesseract
+from PIL import Image
+from datetime import date, datetime
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
-from keyboard import start_keyboard
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from functions import getTime
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+custom_config = os.getenv("CUSTOM_CONFIG")
 
 bot = Bot(token=TOKEN)
+storage = MemoryStorage()
 dp = Dispatcher(bot=bot)
 
 
 @dp.message_handler(commands=["start"])
 async def start(msg: types.Message):
-    await msg.answer("Добро пожаловать!", reply_markup=start_keyboard)
+    await msg.answer("Добро пожаловать! Пришлите своё фото и я обработую его и пришлю в цифровом варианте.\n"
+                     "Данный бот пока что распознают текста только на английском.\n"
+                     "ВНИМАНИЕ!\n"
+                     "Для корректного разпознания текста от пользователя требуется повысить некоторые параметры в изображении!\n"
+                     "А именно контрастность и чёткость! Также можно изменять другие параметры если вам покажется что чёткость изображения повысится")
 
 
 @dp.message_handler(commands=["help"])
-async def start(msg: types.Message):
-    await msg.answer("Доступные вам команды: \n/start \n/help")
+async def help(msg: types.Message):
+    await msg.answer("Доступные вам команды: \n/start \n/help \n/moreInfo")
 
 
-@dp.message_handler(text="Обо мне")
-async def about(msg: types.Message):
-    await msg.reply("Меня зовут Адис")
+@dp.message_handler(commands=["moreInfo"])
+async def info(msg: types.Message):
+    await msg.answer("Если вы обнаружили проблемы некорретной разпознование текста то пишите сюда: @Adis223\n"
+                     "Ошибки которые на данный момент известно:\n"
+                     "Заглавная буква I. Если вы вдруг в тексте использутся шрифт то каждая буква должна различатся между собой!")
 
 
-@dp.message_handler(text="Чем занимаюсь")
-async def about(msg: types.Message):
-    await msg.reply("Занимаюсь я программированием.\n"
-                    "Мне 15 лет и я владею навыкми React, HTML, CSS, JavaScript, Python и немного разбираюсь в работе GitHub :)\n"
-                    "На данный момент учусь создавать ботов чтобы немного подзаработать")
+@dp.message_handler(content_types=types.ContentTypes.PHOTO)
+async def get_photo(msg: types.Message):
+    photo_id = msg.photo[-1].file_id
 
+    minute = getTime(datetime.now().minute)
+    hour = getTime(datetime.now().hour)
+    day = getTime(date.today().day)
+    month = getTime(date.today().month)
+    year = getTime(date.today().year)
 
-@dp.message_handler(text="Контакты")
-async def about(msg: types.Message):
-    await msg.reply("Вы можете со мной связатся через telegram, watsapp, discord\n"
-                    "Discord: adis223\n"
-                    "Watsapp: 996-709-147-518\n"
-                    "Telegram: 996-709-147-518\n")
+    photo_file = await bot.get_file(photo_id)
+    save_path = 'users_images'
+    save_file_path = os.path.join(save_path, f'{year}-{month}-{day}_{hour}.{minute}({msg.from_user.id}).jpg')
+    await photo_file.download(destination_file=save_file_path)
+
+    await msg.answer(pytesseract.image_to_string(Image.open(save_file_path), config=custom_config))
 
 
 if __name__ == '__main__':
